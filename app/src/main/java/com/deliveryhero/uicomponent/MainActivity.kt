7c9ad562da763cb9798fa960d75bb55e0efaeaed
Hardik Shah <47896958+HardikShah1234@users.noreply.github.com>
2022-02-07 04:32:56 +0100

    motion adjusted

--- app/src/main/java/com/deliveryhero/uicomponent/MainActivity.kt
+++ app/src/main/java/com/deliveryhero/uicomponent/MainActivity.kt
@@ -5,7 +5,7 @@ import android.graphics.Typeface
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.style.StyleSpan
-import android.view.View
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -18,41 +18,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Banner Info
-        val expandableTextView = text_layout.findViewById<CustomTextView>(R.id.custom_text_view_deal)
+        val expandableTextView =
+            text_layout.findViewById<CustomTextView>(R.id.custom_text_view_deal)
         val textView = findViewById<TextView>(R.id.textview)
         expandableTextView.setTextColor(Color.WHITE)
         expandableTextView.text = getString(com.deliveryhero.banners.R.string.Text)
 
         textView.setOnClickListener {
-            textView.text = if (expandableTextView.isExpanded) getBoldString("Show More") else getBoldString("Show Less")
+            textView.text =
+                if (expandableTextView.isExpanded) getBoldString("Show More") else getBoldString("Show Less")
             expandableTextView.toggle()
         }
 
         //Banner deal
-        val customTextViewDeal = text_layout_deal.findViewById<CustomTextView>(R.id.custom_text_view_deal)
+        val customTextViewDeal =
+            text_layout_deal.findViewById<CustomTextView>(R.id.custom_text_view_deal)
         val textViewDeal = text_layout_deal.findViewById<TextView>(R.id.textview_deal)
         val imageView = text_layout_deal.findViewById<ImageView>(R.id.display_pic_deal)
         customTextViewDeal.text = getString(R.string.Text)
-
-
         textViewDeal.setOnClickListener {
-            textViewDeal.text = if (customTextViewDeal.isExpanded) getBoldString("Show More") else getBoldString("Show Less")
+            textViewDeal.text =
+                if (customTextViewDeal.isExpanded) {
+                    getBoldString("Show More")
+                } else {
+                    getBoldString("Show Less")
+                }
             customTextViewDeal.toggle()
-        }
-
-        if (customTextViewDeal.lineCount == 1) {
-            imageView.visibility = View.GONE
-            textViewDeal.visibility = View.GONE
-        } else {
-            imageView.visibility = View.VISIBLE
-            textViewDeal.visibility = View.VISIBLE
-
         }
     }
 
-    private fun getBoldString(show: String) : SpannableString {
+    private fun getBoldString(show: String): SpannableString {
         val spannableString: SpannableString = SpannableString(show)
-        spannableString.setSpan(StyleSpan(Typeface.BOLD),0,spannableString.length,0)
+        spannableString.setSpan(StyleSpan(Typeface.BOLD), 0, spannableString.length, 0)
         return spannableString
     }
 }
